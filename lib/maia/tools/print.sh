64d6c60018a0c4c34ae837da5ad34883d702a543
Ola Lundqvist <ola@inguza.com>
2026-08-17 23:47:11 +0000

    Basic print tool. Useful for many things.

---
+++ lib/maia/tools/print.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Copyright (c) 2026 Ola Lundqvist <ola@inguza.com>
+#
+# Licensed under the GNU General Public License v3.0.
+# See LICENSE-GPLv3.txt for the full license text.
+# Commercial licensing is available separately.
+#
+
+set -euo pipefail
+
+. "$MAIA_TOOLS_LIB_DIR/common.sh"
+declare -A param
+parseparam
+
+printf '%b' "${param[content]}"
