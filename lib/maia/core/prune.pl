c04792e46d5f1671a802e5fdc6e153cbf279224b
Ola Lundqvist <ola@inguza.com>
2026-08-17 19:13:00 +0000

    Copyright and license information for prune.pl.

--- lib/maia/core/prune.pl
+++ lib/maia/core/prune.pl
@@ -1,7 +1,12 @@
 #!/usr/bin/env perl
 #
-# Copyright (c) 2025 Ola Lundqvist <ola@inguza.com>
+# Copyright (c) 2025-2026 Ola Lundqvist <ola@inguza.com>
 #
+# Licensed under the GNU General Public License v3.0.
+# See LICENSE-GPLv3.txt for the full license text.
+# Commercial licensing is available separately.
+#
+
 use strict;
 use warnings;
 
