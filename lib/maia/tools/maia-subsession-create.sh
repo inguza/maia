230d30db9a3c00a431a38addeb2a42a288a7c62e
Ola Lundqvist <ola@inguza.com>
2026-08-24 19:14:07 +0000

    Correction of how subsessions are created. The previous approach worked only if the workspace was 'used'.

--- lib/maia/tools/maia-subsession-create.sh
+++ lib/maia/tools/maia-subsession-create.sh
@@ -18,4 +18,12 @@ parseparam
 thissession="$(resolve_session_name)"
 actualsession="$(resolve_subsession_name "${param[name]}")"
 unset MAIA_SESSION
-"$MAIA_BIN" session create "$actualsession" 2>&1 | session_filter "$thissession"
+# A subsession is a clone of the current session, but with the following removed:
+# - history
+# - files
+# No extra filesets
+"$MAIA_BIN" session create "$actualsession" "$thissession" 2>&1 | session_filter "$thissession" | sed 's/ by copying from.*//;'
+export MAIA_SESSION="$actialsession"
+"$MAIA_BIN" session set --extra-send-filesets ""
+"$MAIA_BIN" history clear
+"$MAIA_BIN" file forget "*"
