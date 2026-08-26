0dc1894f2fa284cec5d53a8a3ee75036c4cf5f75
Ola Lundqvist <ola@inguza.com>
2026-08-26 22:11:38 +0000

    Tool corrections.

---
+++ lib/maia/tools/job-delete.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Copyright (c) 2026 Ola Lundqvist <ola@inguza.com>
+#
+# Licensed under the GNU General Public License v3.0.
+# See LICENSE-GPLv3.txt for the full license text.
+# Commercial licensing is available separately.
+#
+
+set -eo pipefail
+
+. "$MAIA_TOOLS_LIB_DIR/common.sh"
+declare -A param
+parseparam
+
+subsession="${param[subsession]:-}"
+if [[ -n "$subsession" ]] ; then
+    set_subsession "$subsession"
+fi
+
+id="${param[id]:-}"
+force="${param[force]:-}"
+
+if [[ -z "$id" ]]; then
+    echo "Missing required parameter: id" >&2
+    exit 1
+fi
+
+# Normalize force to a boolean-like value
+if [[ "$force" == "true" || "$force" == "1" ]]; then
+    "$MAIA_BIN" job delete --force "$id"
+else
+    "$MAIA_BIN" job delete "$id"
+fi
