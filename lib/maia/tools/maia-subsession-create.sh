d6ec0bc367036008a872e3c734f4c7827ce77a33
Ola Lundqvist <ola@inguza.com>
2026-08-29 20:11:55 +0000

    Subsession create use the new configuration attributes #43.

--- lib/maia/tools/maia-subsession-create.sh
+++ lib/maia/tools/maia-subsession-create.sh
@@ -17,13 +17,32 @@ parseparam
 
 thissession="$(resolve_session_name)"
 actualsession="$(resolve_subsession_name "${param[name]}")"
-unset MAIA_SESSION
-# A subsession is a clone of the current session, but with the following removed:
-# - history
-# - files
-# No extra filesets
-"$MAIA_BIN" session create "$actualsession" "$thissession" 2>&1 | session_filter "$thissession" | sed 's/ by copying from.*//;'
-export MAIA_SESSION="$actialsession"
-"$MAIA_BIN" session set --extra-send-filesets ""
-"$MAIA_BIN" history clear
-"$MAIA_BIN" file forget "*"
+# We do not want to unset here, because we want to have workspace copied from the current session
+# unset MAIA_SESSION
+
+# Create blank subsession (do not copy from parent)
+"$MAIA_BIN" session create "$actualsession" 2>&1 | session_filter "$thissession"
+
+export MAIA_SESSION="$actualsession"
+
+# Read config parameters for default subsession setup
+_cfg=$(load_merged_config session)
+
+# Helper function to read config param and run maia command
+apply_config_param() {
+    local param_name="$1"
+    local maia_cmd1="$2"
+    local maia_cmd2="$3"
+    local value=$(jq -r --arg key "$param_name" '.[$key] // ""' <<< "$_cfg")
+    read -ra patterns <<< "$value"
+    if (( ${#patterns[@]} > 0 )); then
+        "$MAIA_BIN" "$maia_cmd1" "$maia_cmd2" --scope "session" "${patterns[@]}"
+    fi
+}
+
+apply_config_param default_subsession_tool_allow tool allow
+apply_config_param default_subsession_tool_restrict tool restrict
+apply_config_param default_subsession_skill_allow skill allow
+apply_config_param default_subsession_skill_restrict skill restrict
+apply_config_param default_subsession_skill_remember skill remember
+apply_config_param default_subsession_skill_forget skill forget
